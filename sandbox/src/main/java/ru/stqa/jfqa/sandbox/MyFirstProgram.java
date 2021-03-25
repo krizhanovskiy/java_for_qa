42c5a0b2d2064217e235b3e2d87a622446ff97b9
Vladimir <713641@gmail.com>
2021-03-25 19:14:35 +0300

    Добавили функцию вывода текста, расчета площади квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/jfqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/jfqa/sandbox/MyFirstProgram.java
@@ -4,6 +4,27 @@ public class MyFirstProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+
+		name("Vladimir");
+		name("Natasha");
+
+		Double lenght = 2.0;
+		System.out.println("Площадь квардрата со стороной " + lenght + " = " + area(lenght));
+
+		Double a = 2.0;
+		Double b = 3.0;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 	}
 
+	public static void name(String name) {
+		System.out.println("Hello " + name);
+	}
+
+	public static double area(Double lenght) {
+		return lenght * lenght;
+	}
+
+	public static double area(Double a , Double b) {
+		return a * b;
+	}
 }
