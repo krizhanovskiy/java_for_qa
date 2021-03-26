8cf92a015cd9689aa1dcdcd9209d7c2057ef9dee
Vladimir <713641@gmail.com>
2021-03-26 10:51:15 +0300

    Методы функции, ассоциированные с объектами

--- sandbox/src/main/java/ru/stqa/jfqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/jfqa/sandbox/MyFirstProgram.java
@@ -11,21 +11,17 @@ public class MyFirstProgram {
 		name("Natasha");
 
 		Square s = new Square(5.0);
-		System.out.println("Площадь квардрата со стороной " + s.l + " = " + area(s));
+		System.out.println("Площадь квардрата со стороной " + s.l + " = " + s.area());
 
 		Rectangle r = new Rectangle(5.0, 6.0);
-		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
 	public static void name(String name) {
 		System.out.println("Hello " + name);
 	}
 
-	public static double area(Square s) {
-		return s.l * s.l;
-	}
 
-	public static double area(Rectangle r) {
-		return r.a * r.b;
-	}
+
+
 }
